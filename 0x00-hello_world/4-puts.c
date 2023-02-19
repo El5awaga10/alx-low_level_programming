25758a8455853e9092fee4747fa94a11fa49c016
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-02-19 14:08:27 -0800

    gf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
-#!/bin/bash
-#include <stdio.h> int main(void) {  int puts(const char *"Programming is like building a multilingual puzzle); return(0); }
+#include <stdio.h>
+int main(void) {  int puts(const char *"Programming is like building a multilingual puzzle); return(0);}
