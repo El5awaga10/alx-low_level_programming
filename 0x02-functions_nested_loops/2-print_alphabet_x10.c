81f014b7416ebcc6851790fd1326ee287b799576
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-02-22 11:17:22 -0800

    fd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,10 @@
 void print_alphabet_x10(void)
 {
 	int i, j;
-	i=0;
-	while(i<10)
+	i = 0;
+	while ( i < 10 )
 	{
-		for(j= 'a';j<= 'z';j++)
+		for ( j = 'a' ; j <= 'z';j++ )
 		{
 			_putchar(j);
 		}
