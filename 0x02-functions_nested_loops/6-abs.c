ec24cbc5a7894cccab1a5f2625765a4822909049
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-02-22 15:50:53 -0800

    ds

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs- absolute value of an integer.
+ * Return: Always 0
+ */
+
+int _abs(int)
+{
+	int i;
+	if (i < 0)
+		_putchar (-i);
+	else if (i > 0)
+		_putchar (i);
+	else (i = 0)
+		_putchar ('0');
+	return (0);
+}
