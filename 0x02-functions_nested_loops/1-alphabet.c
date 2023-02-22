1962ad2038fe2ee78992fc3bd3a96c9bf9f083d5
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-02-22 10:34:00 -0800

    fd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- entry point
+ * prints the alphabet
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i= 0;
+	char c;
+	if (islower(c))
+		while (i!=EOF)
+		{
+			_putchar("%c\n", c);
+			i++;
+		}
+        return (0);
