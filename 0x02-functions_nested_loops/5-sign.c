a5c6a36a3fd042efede37cf66ec795ac16cd34d4
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-02-22 15:35:26 -0800

    fd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  * print_sign- prints the sign of a number.
+ * @n: sign number
  * Return: Always 0
  */
 
@@ -10,7 +11,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar ('+');
-	        return (1);
+		return (1);
 	}
 	else if (n == 0)
 	{
