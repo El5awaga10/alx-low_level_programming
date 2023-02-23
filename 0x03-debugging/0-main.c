cbf657e70c531921a43cc31b412cc94e29db643a
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-02-23 07:03:09 -0800

    gfd

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,27 +3,14 @@
 
 /**
  * main- entry point
- * positive_or_negative- prints if integer is positive or negative
- * @i: the integer
  * Return: Always 0
  */
 
-positive_or_negative(int i)
-{
-	if (i < 0)
-		printf("%d is negative", i);
-	else if (i > 0)
-		printf("%d is positive", i);
-	else
-		printf("%d is null", i);
-	return (0);
-}
 
 int main(void)
 {
-	int i = 98;
+	int i = 0;
 
 	positive_or_negative(i);
-
 	return (0);
 }
