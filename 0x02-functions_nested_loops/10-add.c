4dfebe4d0ce610e2051124725825484796612ae3
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-02-22 17:21:03 -0800

    gf

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add- add two intgers
+ * @a: first int
+ * @b: second int
+ * Return: Always 0
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	_putchar(result);
+	return (0);
