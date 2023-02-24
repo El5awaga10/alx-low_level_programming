bdb709058722ab7c7262063175bd2ed0d6da8595
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-02-23 17:12:14 -0800

    fd

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,9 @@
 void print_numbers(void)
 {
 int number = 0;
-for (number = 0; number < 10; number++)
+for (number = 0; number <= 9; number++)
 {
 _putchar(number);
 }
-_putchar(10);
+_putchar('\n');
 }
