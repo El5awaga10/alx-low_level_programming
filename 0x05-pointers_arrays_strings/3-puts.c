ecd84f287dce73424526bb0225c5ffc3a9f16dce
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-02-28 14:53:52 -0800

    fd

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts- prints a string, followed by a new line
+ * @str: type of string
+ * Return: return nothing
+ */
+
+void _puts(char *str)
+{
+	_putchar(*str '\n');
+
