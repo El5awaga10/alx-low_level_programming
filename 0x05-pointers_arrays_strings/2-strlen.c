abdabb6625b68ab0d4ecbf69c190cab68ce8c75f
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-02-28 14:32:11 -0800

    fd

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen- returns the length of a string.
+ * @s: point to character.
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	strlen(*s);
+	return (0);
+}
