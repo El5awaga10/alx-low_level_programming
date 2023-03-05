2d8eac05cc219e11bdf6130476a8a74893ce509d
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-03-05 08:44:23 -0800

    gfd

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 100; d++)
+	{
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
