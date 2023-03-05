95eeda70629e5647cea196dc7851da7eaea980ba
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-03-05 07:35:14 -0800

    gfd

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,13 @@ int main(void)
 	char c;
 
 	c = 'a';
-       if (c <= 'z')
+       while (c <= 'z')
 	{
-		if (c == 'e' || c == 'q')
+		if (c != 'e' && c != 'q')
 		{
-			continue;
-		}
 		putchar(c);
 		c++;
+		}
 	}
 	putchar('\n');
 	return (0);
