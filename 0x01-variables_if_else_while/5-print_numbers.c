a3c904cc8562082335aa574e537cbe3192a3dbd3
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-03-05 07:42:32 -0800

    gfd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	digit = 0;
+	while (digit < 10)
+	{
+		printf("%d", digit);
+		digit++;
+	}
+	return (0);
+}
