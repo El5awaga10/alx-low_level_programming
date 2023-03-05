08685ea78097bec38d2f6031bf60d4a123c6045c
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-03-05 07:22:54 -0800

    gfd

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z' && c != 'q' && c != 'e')
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
