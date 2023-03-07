ea9f017750754eb801273d8351e75472663709d3
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-03-06 18:27:24 -0800

    gfd

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,12 @@
 
 char *_strchr(char *s, char c)
 {
-	while (c != NULL)
-	{
-		_putchar(c);
-		c++;
-	}
-	return (*s);
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
