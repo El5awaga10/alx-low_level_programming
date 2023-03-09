b5d1d0f32693de1eb111092d88367a3e66368c37
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-03-09 11:33:05 -0800

    fd

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,14 +7,8 @@
  */
 
 int _strlen_recursion(char *s)
-{
-	int i;
-
-	i = 0;
 	if (*s != '\0')
-	{
-		_strlen_recursion(s + 1);
-		i++;
-	}
-	return i;
+		return (1 + _strlen_recursion(s + 1));
+
+	return (0);
 }
