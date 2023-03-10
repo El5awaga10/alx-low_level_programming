e037e2879750d272372a5cfd87eb4eb3078ca2b3
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-03-10 04:09:29 -0800

    fd

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,17 +10,7 @@
 int main(int argc, char *argv[])
 {
 	(void)*argv;
-	int count;
-	int sum = 0;
+	printf("%d\n", argc - 1);
 
-	count = 0;
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-		{
-			sum = sum + count;
-		}
-	}
-	printf("%d\n", sum);
 	return (0);
 }
