2da6c57936e34a602a3c5f0e6a9ab1cfec3da20f
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-03-10 01:57:29 -0800

    gfd

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main- Entry point
+ * @argc: a counter
+ * @argv: an array
+ * Return: return 0
+ */
+
+int main (int argc, char *argv[])
+{
+	printf ("the name of the program is %s", argv[0]);
+	_putchar('\n');
+	return (0);
+}
