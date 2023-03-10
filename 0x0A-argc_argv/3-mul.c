d449e3e0093015879b563f0b328536d766ec72d6
El5awaga10 <mahmoudalkhawaga36@gmail.com>
2023-03-10 06:51:18 -0800

    fd

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main- Entry point
@@ -7,19 +8,13 @@
  * Return: return 0
  */
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
-	int mul;
-
-	if (argc == 0 || argc == 1)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		mul = argv[1] * argv[2];
-		printf("%d\n", mul);
-	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
